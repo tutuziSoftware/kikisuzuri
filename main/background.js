c48a28b275afe686ef2a5eb23be17456d8073aaf
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2016-07-09 16:14:37 +0900

    バルーンのキャンセルボタンを実装

--- main/background.js
+++ main/background.js
@@ -1,31 +1,102 @@
-//ページを開いた時にChrome APIを叩く
+"use strict";
+
+var menus = [
+    {
+        "name":"用事"
+    },
+    {
+        "name":"ゲーム"
+    },
+    {
+        "name":"ニュース"
+    },
+    {
+        "name":"おすすめ/お役立ち"
+    },
+    {
+        "name":"チュートリアル"
+    },
+    {
+        "name":""
+    },
+    {
+        "name":"本体設定"
+    },
+    {
+        "name":"ゴーストの変更"
+    },
+    {
+        "name":"シェルの変更"
+    },
+    {
+        "name":"バルーンの変更"
+    },
+    {
+        "name":""
+    },
+    {
+        "name":"手前に/普通に"
+    },
+    {
+        "name":"どいて"
+    },
+    {
+        "name":"疲れてない？"
+    },
+    {
+        "name":"ニンゲンヤメマスカ",
+        event_name:"cancel",
+        event:"close_balloon"
+    },
+    {
+        "name":""
+    },
+    {
+        name:"キャンセル",
+        event_name:"cancel",
+        event:"close_balloon"
+    }
+];
+
+
+
+
 chrome.runtime.onMessage.addListener((req, _, sendResponse)=>{
-    if(req.action == "get_shell"){
-        //shellの画像を表側へ送信
-        chrome.runtime.getPackageDirectoryEntry((rootPath)=>{
-            rootPath.getFile("kero_412.png", {create:false}, (path)=>{
-                path.file((file)=>{
-                    var reader = new FileReader();
-                    reader.readAsDataURL(file);
-                    reader.onload = (e)=>{
-                        sendResponse({
-                            "shell": e.target.result
-                        });
-                    }
+    var events = {
+        "get_shell":()=>{
+            //shellの画像を表側へ送信
+            chrome.runtime.getPackageDirectoryEntry((rootPath)=>{
+                rootPath.getFile("kero_412.png", {create:false}, (path)=>{
+                    path.file((file)=>{
+                        var reader = new FileReader();
+                        reader.readAsDataURL(file);
+                        reader.onload = (e)=>{
+                            sendResponse({
+                                "shell": e.target.result
+                            });
+                        }
+                    });
                 });
             });
-        });
-    }else if(req.action == "get_context_menu"){
-        //コンテキストメニュー一覧を取得
-        sendResponse([
-            {
-                "name":"時刻"
-            },
-            {
-                "name":"タイマー"
-            }
-        ]);
+        },
+        "get_context_menu":()=>{
+            //コンテキストメニュー一覧を取得
+            sendResponse(menus);
+        }
     }
+
+    if(events[req.action]) events[req.action]();
+
+    //コンテキストメニューの設定項目に応じて動作を返す。
+    menus.filter((menu)=>{
+        if(req.action == menu.event_name){
+            return menu;
+        }
+    }).forEach((menu)=>{
+        sendResponse({
+            method:menu.event
+        });
+    });
 
     //非同期の場合trueにする
     return true;
--- main/interrupt_tab.js
+++ main/interrupt_tab.js
@@ -1,44 +1,105 @@
-//ページを開いた時
+//シェルの表示、イベントの設定
 $(()=>{
+    //シェルの表示領域。
+    //シェルの表示領域は左右に移動できる。
     var shell_space = $("<div>")
                         .css("position", "fixed")
                         .css("bottom", 0)
-                        .css("right", 0);
+                        .css("right", 0)
+                        .draggable({ axis: "x" });
 
+    //シェルの画像をbackground.jsから取得 & 設定
     chrome.runtime.sendMessage({action: "get_shell"}, function(res){
         console.dir(res);
 
         var shell = $("<img>")
                         .attr("src", res.shell)
-                        .bind('contextmenu', create_get_context_menu_listener())
-                        .draggable({ axis: "x" });
+                        .bind('contextmenu', create_get_context_menu_listener());
 
         shell_space
             .append(shell)
             .appendTo("body");
     });
 
+    /**
+     * シェルを右クリックした時のイベントリスナー。
+     * @returns {Function}
+     */
     function create_get_context_menu_listener(){
-        var is_show = false;
-        var $menu = $("<ul>");
 
-        return function(){
-            if(is_show == false){
-                chrome.runtime.sendMessage({action: "get_context_menu"}, function(menus){
-                    menus.forEach((menu)=>{
-                        $menu.append($("<li>").text(menu.name));
-                    });
-
-                    $menu
-                        .css("position", "relative")
-                        .appendTo(shell_space);
-                });
-            }else{
-                $menu.remove();
-                $menu = $("<ul>");
+        /**
+         * メニューを表示する為のクラスです。
+         * メニューの表示・操作はこのクラスで行います。
+         */
+        class MenuAPI{
+            constructor() {
+                this.$menu = $("<ul>");
+                this.is_show = false;
             }
 
-            is_show = !is_show;
+            /**
+             * メニューを表示します。
+             * メニューの項目、および
+             */
+            open_balloon(){
+                var self = this;
+
+                chrome.runtime.sendMessage({action: "get_context_menu"}, (menus)=>{
+                    menus.forEach(function(menu){
+                        var li = $("<li>").text(menu.name);
+                        if(menu.event_name){
+                            li.click(()=>{
+                                chrome.runtime.sendMessage({action:menu.event_name}, (ret)=>{
+                                    self[ret.method]();
+                                });
+                            });
+                        }
+                        this.$menu.append(li);
+                    }, this);
+
+                    //バルーン基本CSS
+                    this.$menu
+                        .css("position", "absolute")
+                        .css("display", "inline-block")
+                        .css("top", "-250%")
+                        .css("left", "-150%")
+                        .css("list-style-type", "square")
+                        .css("padding", "0px 20px 0px 25px")
+                        .css("border", "1px solid black")
+                        .css("border-radius", 10)
+                        .css("text-align", "left")
+                        .find(":empty")
+                        .css("list-style-type", "none")
+                        .css("height", 20)
+
+                    //バルーン追加CSS
+                    this.$menu
+                        .css("background-color", "rgb(61,58,63)")
+                        .css("color", "white")
+                        .appendTo(shell_space);
+                });
+
+                this.is_show == true;
+            }
+
+            close_balloon(){
+                this.$menu.remove();
+                this.$menu = $("<ul>");
+                this.is_show == false;
+            }
+
+            toggle_balloon(){
+                if(this.is_show == false){
+                    this.open_balloon();
+                }else{
+                    this.close_balloon();
+                }
+            }
+        };
+        var menu_api = new MenuAPI;
+
+        return function(){
+            menu_api.toggle_balloon();
 
             return false;
         };
